8c87ad7d911f7365d8dee54a7330b0e8c5677946
hope.bernard <hope.bernard.96@gmail.com>
2023-12-24 19:13:40 +0100

    half

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  var result = 0,
+        n_digits = [];
+    while (n) {
+        n_digits.push(n % 10);
+        n = Math.floor(n / 10);
+    }
+    for (var index_n = 0; index_n < n_digits.length; index_n++) {
+        var n = 0;
+        for (var i = n_digits.length - 1; i >= 0; i--) {
+            if (i !== index_n) {
+                n = n * 10 + n_digits[i];
+            }
+        }
+        result = Math.max(n, result);
+    }
+    return result;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+ 
+  // Create an array to store the first letters of the members' names
+  let dreamTeamName = [];
+ 
+  // Loop through the members array
+  for (let i = 0; i < members.length; i++) {
+    // Check if the current member is a string
+    if (typeof members[i] === 'string') {
+      // Get the first letter of the member's name
+      let firstLetter = members[i].charAt(0).toUpperCase();
+ 
+      // Push the first letter to the dreamTeamName array
+      dreamTeamName.push(firstLetter);
+    }
+  }
+ 
+  // Sort the dreamTeamName array alphabetically
+  dreamTeamName.sort();
+ 
+  // Join the letters in the dreamTeamName array
+  let dreamTeamNameString = dreamTeamName.join('');
+ 
+  // Return the dream team name
+  return dreamTeamNameString;
 }
 
 module.exports = {
